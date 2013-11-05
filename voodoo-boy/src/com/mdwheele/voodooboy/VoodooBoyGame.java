57433580547cba85c1d378257e7087b87e563258
Dustin Wheeler <mdwheele@ncsu.edu>
2013-11-04 21:58:21 -0500

    Updated size to 800x600.

--- voodoo-boy/src/com/mdwheele/voodooboy/VoodooBoyGame.java
+++ voodoo-boy/src/com/mdwheele/voodooboy/VoodooBoyGame.java
@@ -3,33 +3,21 @@ package com.mdwheele.voodooboy;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class VoodooBoyGame implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
 	
 	@Override
-	public void create() {		
-		camera = new OrthographicCamera(1, 1);
-		batch = new SpriteBatch();
+	public void create() {	
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
 	}
 
 	@Override
 	public void render() {		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-
-		batch.end();
 	}
 
 	@Override
